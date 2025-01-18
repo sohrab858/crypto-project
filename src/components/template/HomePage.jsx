8b95c9c138ad9463e45959174922f5526bbb9b23
AliSohrab <alisohrab858@gmial.com>
2025-01-18 16:01:02 +0330

    add curency

--- src/components/template/HomePage.jsx
+++ src/components/template/HomePage.jsx
@@ -2,24 +2,27 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Tablecoin from "../modules/Tablecoin";
 import Pagination from "../modules/Pagination";
+import Search from "../modules/Search";
 import { getCoinList } from "../../services/crypto";
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [page, setPage] = useState(1);
+  const [currency, setCurrency] = useState("usd");
   useEffect(() => {
     setIsLoading(true);
     const getData = async () => {
-      const res = await fetch(getCoinList(page));
+      const res = await fetch(getCoinList(page, currency));
       const json = await res.json();
       setCoins(json);
       setIsLoading(false);
     };
     getData();
-  }, [page]);
+  }, [page, currency]);
   return (
     <div>
+      <Search currency={currency} setCurrency={setCurrency} />
       <Tablecoin coins={coins} isLoading={isLoading} />
       <Pagination page={page} setPage={setPage} />
     </div>
--- src/services/crypto.js
+++ src/services/crypto.js
@@ -1,8 +1,8 @@
 const BASE_URL = "https://api.coingecko.com/api/v3";
 const KEY = "CG-5v3UMmpfqKDahRC9Ra2eUa8n";
 
-const getCoinList = (page) => {
-  return `${BASE_URL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=${page}&x_cg_demo_api_key=${KEY}`;
+const getCoinList = (page, currency) => {
+  return `${BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=20&page=${page}&x_cg_demo_api_key=${KEY}`;
 };
 
 export { getCoinList };
