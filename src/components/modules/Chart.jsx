9ffd0d51ea31d83e0a67375bb94d0631cdf3bafe
AliSohrab <alisohrab858@gmial.com>
2025-01-29 09:55:13 +0330

    add chart4

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./Chart.module.css";
 import { convertData } from "../../helpers/convertData";
 import { useState } from "react";
+import { CartesianGrid, ResponsiveContainer } from "recharts";
+import { LineChart } from "recharts";
 function Chart({ chart, setChart }) {
   const [type, setType] = useState("prices");
   console.log(convertData(chart, type));
@@ -10,7 +12,15 @@ function Chart({ chart, setChart }) {
       <span className={styles.cross} onClick={() => setChart(null)}>
         X
       </span>
-      <div className={styles.chart}></div>
+      <div className={styles.chart}>
+        <div className={styles.graph}>
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart width={400} height={400} data={convertData(chart, type)}>
+              <CartesianGrid stroke="#404042"></CartesianGrid>
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
     </div>
   );
 }
